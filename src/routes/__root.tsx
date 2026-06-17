b56dd50e858d67a1d031cfc0f7d02c4d8d75b20e
huseinbarkah <husein12332111@gmail.com>
2026-06-17 19:49:43 +0800

    ganti file fav ico

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -18,9 +18,10 @@ export const Route = createRootRoute({
       { property: 'og:site_name', content: 'Husein Barkah Pambudi' },
     ],
     links: [
-      { rel: 'icon', href: '/favicon.ico', sizes: '48x48' },
-      { rel: 'icon', type: 'image/png', sizes: '192x192', href: '/logo192.png' },
-      { rel: 'apple-touch-icon', href: '/logo192.png' },
+      { rel: 'icon', type: 'image/png', href: '/favicon-96x96.png', sizes: '96x96' },
+      { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
+      { rel: 'shortcut icon', href: '/favicon.ico' },
+      { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
       { rel: 'manifest', href: '/manifest.json' },
       { rel: 'stylesheet', href: appCss },
       { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
